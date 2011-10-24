ef355178c74e19ff2a2dfa7e501b8a7df9322ea3
Michal Kuklis <michal.kuklis@gmail.com>
2011-10-23 22:27:05 -0400

    more work on spec

--- spec/README.md
+++ spec/README.md
@@ -5,7 +5,18 @@ In order to run specs:
 1. install jasmine-node with:
 
         npm install jasmine-node
-2. run specs (from backbone-nowjs root folder) with:
+
+2. install websocket-client
+        
+        npm install websocket-client
+
+3. install nowclient
+        
+        git clone https://github.com/Flotype/nowclient.git
+        cd nowclient
+        npm install -g
+        npm link nowclient (from backbone-nowjs folder)
+
+4. run specs (from backbone-nowjs root folder) with:
 
         jasmine-node spec
-
--- spec/backbone-nowjs.spec.js
+++ spec/backbone-nowjs.spec.js
@@ -1,10 +1,15 @@
-var Backbone = require('backbone-nowjs');
-var http = require('http');
+var Backbone = require('backbone-nowjs')
+  , http = require('http')
+  , nowjs = require("now")
+  , nowclient = require('nowclient');
 
 (function() {
   beforeEach(function () {
     this.server = http.createServer();
     this.server.listen(1337, "127.0.0.1");
+    this.everyone = nowjs.initialize(this.server);
+    this.list = new Backbone.Backend();
+    Backbone.connector.connect(nowjs, this.everyone, {list: this.list});
   });
 
   afterEach(function () {
@@ -13,6 +18,7 @@ var http = require('http');
 
   describe("backbone-nowjs", function () {
     it("should read from backend", function() {
+
     });
   });
 })();
